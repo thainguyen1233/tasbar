ab4422eec75ad86c8fde9a0ef61a6426e41e2d2e
thainguyen123dm <thainguyen123dm@gmail.com>
2023-12-01 21:55:01 +0700

    no message

---
+++ man.js
@@ -0,0 +1,20 @@
+confirm('nội dung phù hợp với bạn:')
+var a = 'van thai'
+var b =  'nguyen'
+ if (a == b ){
+    console.log('kết quả sẽ cho là đúng :')
+
+}else{
+    console.error('cảnh bảo vì đăng nhập sai:')
+}
+if(a > 0 && b > 0){
+    console('kết quả đăng nhập bị sai:0')
+}else{
+    console.log('kết quả đăng nhập bị đúng:')
+}
+
+setTimeout(function(){
+ var fullName = 'vanthai'
+ console.log(fullName)
+ 
+},10000);
